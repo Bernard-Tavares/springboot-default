0ce02a8c8309b622d19b83e38db5e5136090bba6
Bernard tavares <bernard@sonner.com.br>
2023-07-21 11:01:31 +1100

    projeto criado

---
+++ src/test/java/com/dvision/spring/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dvision.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
