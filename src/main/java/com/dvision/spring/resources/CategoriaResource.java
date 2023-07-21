81dd9cbb0315bad8aa461f2b388c0347172ed3eb
Bernard tavares <bernard@sonner.com.br>
2023-07-21 11:30:39 +1100

    Criacao da primeira classe

--- src/main/java/com/dvision/spring/resources/CategoriaResource.java
+++ src/main/java/com/dvision/spring/resources/CategoriaResource.java
@@ -1,16 +1,28 @@
 package com.dvision.spring.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.dvision.spring.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "Rest esta funcionando";
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1, "Informatica");
+		Categoria cat2 = new Categoria(1, "Escritorio");
+		
+		List<Categoria> list = new ArrayList<>();
+		list.add(cat1);
+		list.add(cat2);
+		return list;
 	}
 	
 
